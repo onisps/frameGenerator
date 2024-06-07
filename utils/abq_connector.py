7d074e3f0fcd5e105c0cf5f4bd7905e6dc481132
onisps <onis.pavel@gmail.com>
2024-06-07 13:40:26 +0700

    [feat] 1. add self-contact feature. 2. fix abaqus .cae generator. 3. fix repeat=0 error

--- utils/abq_connector.py
+++ utils/abq_connector.py
@@ -2,13 +2,14 @@ from abaqus import *
 from abaqusConstants import *
 from caeModules import *
 
-
 ## Model
 # create model
 model = mdb.Model('Expand_frame')
 # draw arc
+balloon_dia = 18.4 / 2
+frame_dia = 18 / 2
 sketch = model.ConstrainedSketch(name='balloon', sheetSize=1.0)
-sketch.ArcByCenterEnds(center=(0, 0), point1=(3.3, 0), point2=(3.3, 0), direction=CLOCKWISE)
+sketch.ArcByCenterEnds(center=(0, 0), point1=(balloon_dia, 0), point2=(balloon_dia, 0), direction=CLOCKWISE)
 # set part
 part = model.Part(name='ballon', dimensionality=THREE_D, type=DEFORMABLE_BODY)
 
@@ -19,56 +20,90 @@ part.BaseShellExtrude(sketch=sketch, depth=30)
 geom_file = mdb.openStep(fileName='../geoms/cyl3.stp')
 part2 = model.PartFromGeometryFile(name='frame', geometryFile=geom_file, dimensionality=THREE_D, type=DEFORMABLE_BODY)
 
-
 ## Mesh balloon
 # set elem type
 part.setElementType(regions=(part.faces,), elemTypes=[mesh.ElemType(elemCode=SFM3D4)])
-plane = part.DatumPlaneByThreePoints(point1=(-3.3,0,0), point2=(3.3,0,0), point3=(3.3,0,30)).id
-datum_plane = part.datums[plane]
-part.PartitionFaceByDatumPlane(faces=part.faces, datumPlane=datum_plane)
 
 # set number of element per edge
-part.seedEdgeByNumber(edges=part.edges.findAt(coordinates=((3.3, 0, 1.0), (-3.3, 0, 1.0))), number=1)
-part.seedEdgeByNumber(edges=part.edges.findAt(coordinates=((0, 3.3, 0.0), (0, 3.3, 30.0))), number=100)
+part.seedEdgeByNumber(edges=part.edges.findAt(coordinates=((balloon_dia, 0, 1.0), (-balloon_dia, 0, 1.0))), number=1)
+part.seedEdgeByNumber(edges=part.edges.findAt(coordinates=((0, balloon_dia, 0.0), (0, balloon_dia, 30.0))), number=100)
 # create mesh
 part.generateMesh()
 
 ## Create sets
 # set for section assingment
-part.Set(name='set-all', faces=part.faces.findAt(coordinates=((3.3, 0, 0.0),)))
+part.Set(name='set-all', faces=part.faces.findAt(coordinates=((balloon_dia, 0, 0.0),)))
 # surface for  BC and contact
 part.Surface(name='surface-contact',
-             side2Faces=part.faces.findAt(coordinates=((3.3, 0, 0),)),)
-
+             side1Faces=part.faces.findAt(coordinates=((balloon_dia, 0, 0),)), )
 
 ## Mesh Frame
 # set elem type
 part2.createVirtualTopology(regions=(part2.faces, part2.edges, part2.nodes, part2.cells),
-                            # smallFaceAreaThreshold=0.02, mergeSmallFaces=TRUE,
-                            # faceAspectRatioThreshold=0.002, mergeSliverFaces=TRUE,
-                            # smallFaceCornerAngleThreshold=10, mergeSmallAngleFaces=TRUE,
-                            # thinStairFaceThreshold=0.004, mergeThinStairFaces=TRUE,
                             ignoreRedundantEntities=TRUE
                             )
 part2.setElementType(regions=(part2.cells,), elemTypes=[mesh.ElemType(elemCode=C3D8)])
-part2.seedPart(size=0.15)
+part2.seedPart(size=0.2)
 # # set number of element per edge
 # part.seedEdgeByNumber(edges=part.edges.findAt(coordinates=((3.3, 0, 1.0), (-3.3, 0, 1.0))), number=1)
 # part.seedEdgeByNumber(edges=part.edges.findAt(coordinates=((0, 3.3, 0.0), (0, 3.3, 30.0))), number=50)
+
 # create mesh
 part2.generateMesh()
 
 part2.Set(name='set-cells', cells=part2.cells)
-# coords = ()
-part2.Surface(name='surface-contact', side12Faces=part2.faces.findAt(coordinates=((3.5,0,5),(3.5,0,-5),(3.5,0,10),(3.5,0,-10),)),)
-# part2.Surface(name='sur-test', side1Elements=part2.elemFaces.getByBoundingCylinder(center1=(0,0,-20), center2=(0,0,20), radius=3.5),)
-# part2.Set(name='surface-contact', nodes=part2.nodes.getByBoundingCylinder(center1=(0,0,-20), center2=(0,0,20), radius=3.5) ,)
-# nodes = part2.faces.findAt(coordinates=((3.5,0,5),(3.5,0,-5),))
-part2.Set(name='sur-test', nodes=part2.nodes.getByBoundingCylinder(center1=(0,0,-20), center2=(0,0,20), radius=3.5) ,)
-# vert = part2.vertices.getClosest(coordinates=((3.5,0,0),),)
-# vert = part2.vertices.getClosest(coordinates=((3.5,0,0), (3.7,0,0),),)
-# part2.Set(name='fixed_node', vertices= part2.vertices.getClosest(coordinates=((3.5,0,0), (4,0,0),), searchTolerance=5),)
-# part2.Set(name='fixed_node', vertices= part2.vertices.getByBoundingBox(xMin=-4, yMin=-4, zMin=-2.5, xMax=4, yMax=4, zMax=2.5),)
+
+surfaces_all = part2.Surface(name='all_faces', side2Faces=part2.faces)
+part2.Set(name='sur-test',
+          nodes=part2.nodes.getByBoundingCylinder(center1=(0, 0, -10), center2=(0, 0, 10), radius=frame_dia), )
+t_faces = part2.faces.getClosest(
+                  coordinates=
+                  (
+                      (frame_dia+0.1, 0, 5),
+                      (frame_dia+0.1, 0, -5),
+                  ),
+              )
+coords = list()
+for i in range(20):
+    coords.append((frame_dia, 0, 0.5*i))
+surfaces_outer = part2.Surface(name='surface-contact', side2Faces=part2.faces.findAt(
+    coordinates=coords
+    # (
+    #     (frame_dia, 0, -10),
+    #     (frame_dia, 0, -9),
+    #     (frame_dia, 0, -8),
+    #     (frame_dia, 0, -7),
+    #     (frame_dia, 0, -6),
+    #     (frame_dia, 0, -5),
+    #     (frame_dia, 0, -4),
+    #     (frame_dia, 0, -3),
+    #     (frame_dia, 0, -2),
+    #     (frame_dia, 0, -1),
+    #     (frame_dia, 0, 0),
+    # )
+))
+surfaces_inner = part2.Surface(name='inner_faces',
+                               side2Faces=part2.faces.getByBoundingCylinder(
+                                   center1=(0, 0, 10),
+                                   center2=(0, 0, -10),
+                                   radius=frame_dia-0.5)
+                               )
+
+print('surf all:', surfaces_all)
+print('surf out:', surfaces_outer)
+print('surf in:', surfaces_inner)
+
+surfaces_self_contact = part2.SurfaceByBoolean(
+    name='self-contact',
+    surfaces=[surfaces_all, surfaces_inner, surfaces_outer],
+    operation=DIFFERENCE
+)
+part2.SurfaceByBoolean(
+    name='surface-contact',
+    surfaces=[surfaces_all, surfaces_inner, surfaces_self_contact],
+    operation=DIFFERENCE
+)
+
 ## Assembly
 # create cylindrical coordinate system
 csys = model.rootAssembly
@@ -78,15 +113,14 @@ datum = csys.DatumCsysByThreePoints(
     line1=(0, 0, 1), line2=(0, 1, 0), name='test').id
 
 # create assembly
-model.rootAssembly.Instance(name='balloon', part=part, dependent=ON).translate(vector=(0,0,-15))
+model.rootAssembly.Instance(name='balloon', part=part, dependent=ON).translate(vector=(0, 0, -15))
 model.rootAssembly.Instance(name='frame', part=part2, dependent=ON)
-
 
 # Material
 material = model.Material(name='material_NiTi')
-material.Elastic(table=((233000., 0.35), ))
-material.Plastic(table=((414.,    0.),
-                        (933., 0.445), ))
+material.Elastic(table=((233000., 0.35),))
+material.Plastic(table=((414., 0.),
+                        (933., 0.445),))
 
 ## Section
 # ballon section
@@ -97,27 +131,28 @@ part2.SectionAssignment(region=part2.sets['set-cells'], sectionName='section_fra
 
 ## Step
 model.StaticStep(name='Step-Load', previous='Initial', description='',
-                        timePeriod=1.0, timeIncrementationMethod=AUTOMATIC,
-                        maxNumInc=200, initialInc=0.0025, minInc=1E-06, maxInc=0.1, nlgeom=ON)
+                 timePeriod=1.0, timeIncrementationMethod=AUTOMATIC,
+                 maxNumInc=1000, initialInc=0.01, minInc=1E-06, maxInc=0.1, nlgeom=ON)
 
 ## Output request
 model.FieldOutputRequest('F-Output-1', createStepName='Step-Load', timeInterval=0.025,
-                                 variables=('S', 'E', 'U'))
+                         variables=('S', 'E', 'U'))
 
 ## Boundary condition
 # create Amplitude
 amp_name = 'Ampl-expand'
-model.TabularAmplitude(name=amp_name, data=((0,0),(0.5,1),(1,0),),)
+model.TabularAmplitude(name=amp_name, data=((0, 0), (0.75, 1), (1, 0.75),), )
 # define boundary to set BC
 expanding_disp = model.rootAssembly.instances['balloon'].sets['set-all']
 # create BC in specific coordinate system
 model.DisplacementBC(name='BC-1', createStepName='Step-Load', localCsys=csys.datums[datum], amplitude=amp_name,
-                          region=expanding_disp, u1=5.2, u2=0, u3=0)
+                     region=expanding_disp, u1=-5.2, u2=0, u3=0)
 
 ## interaction
 # set_frame = model.rootAssembly.instances['frame'].surfaces['surface-contact']
-set_frame = model.rootAssembly.instances['frame'].sets['sur-test']
+set_frame = model.rootAssembly.instances['frame'].surfaces['surface-contact']
 set_balloon = model.rootAssembly.instances['balloon'].surfaces['surface-contact']
+set_frame_self_contact = model.rootAssembly.instances['frame'].surfaces['self-contact']
 prop = model.ContactProperty(name='InterProp')
 prop.TangentialBehavior(formulation=PENALTY, table=((0.2,),), fraction=0.005, )
 prop.NormalBehavior(pressureOverclosure=HARD, )
@@ -128,10 +163,13 @@ model.SurfaceToSurfaceContactStd(name='contact_test', createStepName='Step-Load'
                                  sliding=FINITE, interferenceType=NONE,
                                  interactionProperty='InterProp', enforcement=NODE_TO_SURFACE)
 
+model.SelfContactStd(name='self-contact-frame', createStepName='Step-Load',
+                     surface=set_frame_self_contact,
+                     interactionProperty='InterProp', enforcement=SURFACE_TO_SURFACE)
+
 ## Job
 job = mdb.Job(name='a_Ballon_expand', model='Expand_frame', numCpus=16, numDomains=16, multiprocessingMode=THREADS)
 job.writeInput()
-
 
 ## Submit the job
 
@@ -142,7 +180,8 @@ job.writeInput()
 mdb.saveAs('a_compression.cae')
 
 import os, glob
-for f in glob.glob("abaqus*"):
-    os.remove(f)
-for f in glob.glob("*.sat"):
-    os.remove(f)
+
+# for f in glob.glob("abaqus*"):
+#     os.remove(f)
+# for f in glob.glob("*.sat"):
+#     os.remove(f)
--- utils/cad_drawer.py
+++ utils/cad_drawer.py
@@ -1,59 +1,150 @@
 import cadquery as cd
+import numpy as np
 from jupyter_cadquery.viewer.client import show, show_object
+import matplotlib.colors as mcolors
+import time
 
 
-def model_drawer(radius=4, height=20, repeat=5, cell_size_height=2, cell_size_rad=0.2, cut_space=1.0):
+def model_drawer(
+        diameter=18,
+        height=20,
+        repeat=7,
+        cell_size_height=8,
+        cell_size_rad=0.2,
+
+):
+    padding = 0.5
+    radius = diameter / 2
     cyl_out = cd.Workplane('XY').cylinder(height=height, radius=radius, direct=cd.Vector((0, 0, 1)))
     cyl_cut = cd.Workplane('XY').cylinder(height=height, radius=radius - 0.5, direct=cd.Vector((0, 0, 1)))
     cyl = cyl_out.cut(cyl_cut)
+
+    # pilon sketch pattern
     # s = (
     #     cd.Sketch().arc((0, 0), cell_size_rad, 0.0, 360.0)
     #     .arc((0, cell_size_height), cell_size_rad, 0.0, 360.0)
     #     .hull()
     # )
-    s = cd.Sketch().arc((0, 0), cell_size_rad, 0.0, 360.0).segment((cell_size_height / 2, cell_size_height / 2),
-                                                                   (-cell_size_height / 2, cell_size_height / 2)).arc(
-        (0, cell_size_height), cell_size_rad, 0.0, 360.0).hull().clean()
 
+    # rhombus sketch pattern
+    # s = cd.Sketch().arc((0, 0), cell_size_rad, 0.0, 360.0).segment((cell_size_height / 2, cell_size_height / 2),
+    #                                                                (-cell_size_height / 2, cell_size_height / 2)).arc(
+    #     (0, cell_size_height), cell_size_rad, 0.0, 360.0).hull().clean()
+
+    # hex sketch pattern
+    ang1 = np.deg2rad(45) / 2
+    spacing = 0.2
+    e1 = 0.75
+    h = cell_size_height
+    e2 = 1
+    ang2 = np.deg2rad(45) / 2 / (e2 / e1)
+    s = (cd.Sketch()
+         .segment((-spacing / 2, 0), (spacing / 2, 0))
+         .segment((-np.cos(ang1) * e1, np.sin(ang1) * e1), (np.cos(ang1) * e1, np.sin(ang1) * e1))
+         .segment((-np.cos(ang2) * e2, h - np.sin(ang2) * e2), (np.cos(ang2) * e2, h - np.sin(ang2) * e2))
+         .segment((-spacing / 2, h), (spacing / 2, h)).hull().vertices().fillet(0.5).clean()
+         )
+
+    # s = (cd.Sketch()
+    #      .segment((np.cos(ang1) * e1, -np.sin(ang1) * e1), (-spacing, 0))
+    #      .segment((spacing, 0), (np.cos(ang1) * e1, np.sin(ang1) * e1))
+    #      .segment((h - np.cos(ang2) * e2, -np.sin(ang2) * e2), (h, -spacing))
+    #      .segment((h, spacing), (h - np.cos(ang2) * e2, np.sin(ang2) * e2)).clean()
+    #      .hull().vertices().fillet(0.2)
+    #      )
+    # show_object(s)
+    # return
     # show(s)
     mesh = cd.Workplane('XY')
     mesh_sketch = cd.Workplane('XY')
-    rot = 180 / repeat
-    mesh = (
-        mesh_sketch.union(
-            mesh_sketch
-            .workplane()
-            .placeSketch(s)
-            .extrude(2 * radius)
-            .rotate((0, 0, 0), (1, 0, 0), 90)
-            .translate((0, radius, -height / 2))
+    if repeat > 0:
 
-        )
-    )
-    mesh_pattern = mesh
-    for i in range(int(repeat)):
-        mesh_pattern = mesh_pattern.union(
-            mesh.rotate((0, 0, 0), (0, 0, 1), i * rot)
-        )
-    # show(cyl, mesh_pattern)
+        rot = 180 / repeat
+        mesh = (
+            mesh_sketch.union(
+                mesh_sketch
+                .workplane()
+                .placeSketch(s)
+                .extrude(2 * radius)
+                .rotate((0, 0, 0), (1, 0, 0), 90)
+                .translate((0, radius, -2*height / 4 + padding))
 
-    for hi in range(int(height / (cut_space*cell_size_height))):
-        cyl = (
-            cyl.cut(
-                mesh_pattern
-                .rotate((0, 0, 0), (0, 0, 1), rot * i)
             )
         )
-        if hi % 2 == 1:
-            sight = 1
-        else:
-            sight = -1
-        mesh_pattern = (
-            mesh_pattern
-            .translate((0, 0, cut_space*cell_size_height))
-            .rotate((0, 0, 0), (0, 0, 1), sight*rot / 2)
-        )
-        # show(cyl)
+        mesh_pattern = mesh
 
-    cd.exporters.export(cyl, './geoms/cyl2.stp','STEP')
-    show(cyl)
+
+        for i in range(int(repeat)):
+                mesh_pattern = mesh_pattern.union(
+                    mesh.rotate((0, 0, 0), (0, 0, 1), i * rot)
+                )
+
+        mesh_pattern_inv = (mesh_pattern.rotate((0,0, -height / 2), (0,1, -height / 2), 180)
+                            .translate((0, 0, -cell_size_height/2))
+                            .rotate((0, 0, 0), (0, 0, 1), rot / 2))
+            # show(cyl, mesh_pattern)
+
+        for flag in [0, 1]:
+            if flag == 0:
+                mesh_to_cut = mesh_pattern
+            else:
+                mesh_to_cut = mesh_pattern_inv
+            for hi in range(int(np.floor(height / (cell_size_height*0.5 + padding*2)))):
+                cyl = (
+                    cyl.cut(
+                        mesh_to_cut
+                        # .rotate((0, 0, 0), (0, 0, 1), rot * i)
+                    )
+                )
+                if hi % 2 == 1:
+                    sight = 1
+                else:
+                    sight = -1
+                mesh_to_cut = (
+                    mesh_to_cut
+                    .translate((0, 0, padding + cell_size_height))
+                    # .rotate((0, 0, 0), (0, 0, 1), sight * rot / 2)
+                )
+                # show(cyl, mesh_to_cut)
+                # time.sleep(1)
+    else:
+        # Extract points from the sketch for wrapping
+
+        # Project the wrapped sketch onto the cylindrical surface and extrude
+        mesh = (cd.Workplane('XY')
+                .circle(radius)
+                .extrude(height)
+                .faces(">Z")
+                .workplane()
+                .add(s)
+                .extrude(1)  # Example extrusion depth
+                )
+        show(cyl, mesh)
+        mesh = (
+            mesh_sketch.union(
+                mesh_sketch
+                .workplane()
+                .placeSketch(s)
+                .extrude(2 * radius)
+                .rotate((0, 0, 0), (-1, 0, 0), 90)
+                .translate((0, -radius, -(0.5*height-0.5*cell_size_height)))
+                # .translate((0, -radius, -(0.5*height+0.5*cell_size_height+padding)))
+
+            )
+        )
+        mesh_to_cut = mesh
+        for hi in range(int(np.floor(height / (cell_size_height*0.5 + padding*2)))):
+            cyl = (
+                cyl.cut(
+                    mesh_to_cut
+                    # .rotate((0, 0, 0), (0, 0, 1), rot * i)
+                )
+            )
+            mesh_to_cut = (
+                mesh
+                .translate((0, 0, hi*(padding + cell_size_height)))
+            )
+            # show(cyl, mesh_to_cut)
+            # time.sleep(1)
+    cd.exporters.export(cyl, './geoms/cyl3.stp','STEP')
+    show_object(cyl)  # , name=('Cyl','Sketch'))#, options=dict(alpha=1,color='#C0C0C0'))
